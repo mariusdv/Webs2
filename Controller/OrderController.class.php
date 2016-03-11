ffbb8787fe3a707e7677860b36b07f92d9c93770
Marius de Vogel <mvogel@student.avans.nl>
2016-03-11 12:29:27 +0100

    CART!

--- Controller/OrderController.class.php
+++ Controller/OrderController.class.php
@@ -30,8 +30,36 @@ class OrderController
             header("Location: http://" . $_SERVER['HTTP_HOST'] . $_SERVER[REQUEST_URI]);
             exit(0);
         }
+
+        if (Empty($_GET["action"])) {
+            $this->showCart();
+        } else {
+            switch (strtolower($_GET["action"])) {
+                case "order":
+                    $this->order();
+                    break;
+                default:
+                    $this->showCart();
+                    break;
+            }
+        }
+
+    }
+
+    public function showCart()
+    {
         render("cart.php", ["title" => "Mijn Winkelmandje",
             "message" => $this->message]);
+    }
+
+    public function order()
+    {
+
+        guaranteeLogin("/Order/action=order");
+
+        render("order.php", ["title" => "Mijn Winkelmandje2",
+           "user" => $_SESSION["user"]]);
+
     }
 
 }
--- Includes/functions.php
+++ Includes/functions.php
@@ -1,97 +1,91 @@
 <?php
 
-    require_once("constants.php");
+require_once("constants.php");
 
-    /**
-     * return error message
-     */
-    function apologize($message)
-    {
-       $err = new ErrorController();
-        $err->message = $message;
-        $err->render();
-        exit;
+/**
+ * return error message
+ */
+function apologize($message)
+{
+    $err = new ErrorController();
+    $err->message = $message;
+    $err->render();
+    exit;
+}
+
+
+/**
+ * Redirects user to destination, which can be
+ * a URL or a relative path on the local host.
+ *
+ * Because this function outputs an HTTP header, it
+ * must be called before caller outputs any HTML.
+ */
+function redirect($destination)
+{
+    // handle URL
+    if (preg_match("/^https?:\/\//", $destination)) {
+        header("Location: " . $destination);
+    } // handle absolute path
+    else if (preg_match("/^\//", $destination)) {
+        $protocol = (isset($_SERVER["HTTPS"])) ? "https" : "http";
+        $host = $_SERVER["HTTP_HOST"];
+        header("Location: $protocol://$host$destination");
+    } // handle relative path
+    else {
+        // adapted from http://www.php.net/header
+        $protocol = (isset($_SERVER["HTTPS"])) ? "https" : "http";
+        $host = $_SERVER["HTTP_HOST"];
+        $path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
+        header("Location: $protocol://$host$path/$destination");
     }
 
+    // exit immediately since we're redirecting anyway
+    exit;
+}
+
+function guaranteeLogin($page)
+{
+    $controller = new AccountController();
+    $controller->guaranteeLogin($page);
+}
+
+/**
+ * Renders template, passing in values.
+ */
+function render($template, $values = [])
+{
+    // if template exists, render it
+    if (file_exists("View/$template")) {
+        // extract variables into local scope
+        $smarty = new Smarty();
+        $smarty->assign($values);
+
+        if (!empty($_SESSION["user"]))
+            $smarty->assign("user", $_SESSION["user"]);
+
+        $cart = new Cart();
+        $cart->ItemCount();
+        $smarty->assign("cartSize", $cart->ItemCount());
+        $smarty->assign("cart", $cart->getCart());
+        $smarty->assign("totalPrice", $cart->getTotal());
+
+        $c = new Catalogue();
+        $smarty->assign("categories", $c->getCategories());
 
 
-    /**
-     * Redirects user to destination, which can be
-     * a URL or a relative path on the local host.
-     *
-     * Because this function outputs an HTTP header, it
-     * must be called before caller outputs any HTML.
-     */
-    function redirect($destination)
-    {
-        // handle URL
-        if (preg_match("/^https?:\/\//", $destination))
-        {
-            header("Location: " . $destination);
-        }
+        // render header
+        $smarty->display("View/header.php");
 
-        // handle absolute path
-        else if (preg_match("/^\//", $destination))
-        {
-            $protocol = (isset($_SERVER["HTTPS"])) ? "https" : "http";
-            $host = $_SERVER["HTTP_HOST"];
-            header("Location: $protocol://$host$destination");
-        }
+        // render template
+        $smarty->display("View/$template");
 
-        // handle relative path
-        else
-        {
-            // adapted from http://www.php.net/header
-            $protocol = (isset($_SERVER["HTTPS"])) ? "https" : "http";
-            $host = $_SERVER["HTTP_HOST"];
-            $path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
-            header("Location: $protocol://$host$path/$destination");
-        }
-
-        // exit immediately since we're redirecting anyway
-        exit;
+        // render footer
+        $smarty->display("View/footer.php");
+    } // else err
+    else {
+        trigger_error("Invalid template: $template", E_USER_ERROR);
     }
-
-    /**
-     * Renders template, passing in values.
-     */
-    function render($template, $values = [])
-    {
-        // if template exists, render it
-        if (file_exists("View/$template"))
-        {
-            // extract variables into local scope
-            $smarty = new Smarty();
-            $smarty->assign($values);
-
-            if(!empty($_SESSION["user"]))
-                $smarty->assign("user", $_SESSION["user"]);
-
-            $cart = new Cart();
-            $cart->ItemCount();
-            $smarty->assign("cartSize", $cart->ItemCount());
-            $smarty->assign("cart", $cart->getCart());
-            $smarty->assign("totalPrice", $cart->getTotal());
-
-            $c = new Catalogue();
-            $smarty->assign("categories", $c->getCategories());
-
-
-            // render header
-            $smarty->display("View/header.php");
-
-            // render template
-            $smarty->display("View/$template");
-
-            // render footer
-            $smarty->display("View/footer.php");
-        }
-
-        // else err
-        else
-        {
-            trigger_error("Invalid template: $template", E_USER_ERROR);
-        }
-    }
+}
 
 ?>
