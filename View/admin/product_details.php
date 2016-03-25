d9770f9da83e84293c3c233a8416777cad332474
Marius de Vogel <mvogel@student.avans.nl>
2016-03-25 14:23:58 +0100

    bugfixes

--- View/admin/product_details.php
+++ View/admin/product_details.php
@@ -1,28 +1,34 @@
-<!-- Page Content -->
 <div class="container">
 
-    <div class="row">
-
-        <div class="col-sm-3 col-lg-3">
+    <div class="row flat-header">
+        <div class="col-sm-3 col-lg-3 ">
             <div class="header">Categories</div>
+        </div>
+        <div class="col-sm-9 col-lg-9">
+            <div class="header">{$title}</div>
+        </div>
+    </div>
+    <div class="row flat-lighterblue">
+        <br>
+        <div class="col-sm-3 col-lg-3">
             <div class="panel-group" id="accordion">
                 {foreach from=$categories item=row}
                 <div class="panel">
                     <div class="panel-heading">
                         <h4 class="panel-title">
-                            <a data-toggle="collapse" data-parent="#accordion" href="#collapse{$row->Id}">{$row->Category}</a>
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapse{$row->FoldId}">{$row->Category}</a>
                         </h4>
                     </div>
-                    <div id="collapse{$row->Id}" class="panel-collapse collapse">
+                    <div id="collapse{$row->FoldId}" class="panel-collapse collapse">
                         <div class="panel-body category">
-                            <a href="/catalogue/cat=#">
+                            <a href="/catalogue/cat={$row->Category}">
                                 <div class="subcategory">
                                     All
                                 </div>
                             </a>
                             <hr class="small">
                             {foreach from=$row->SubCategories item=subcat}
-                            <a href="/catalogue/cat={$subcat}">
+                            <a href="/catalogue/subcat={$subcat}">
                                 <div class="subcategory">
                                     {$subcat}
                                 </div>
@@ -37,11 +43,61 @@
         </div>
 
         <div class="col-md-9">
-            PRODUCT DETAILS
+
+            <div class="thumbnail clean">
+                <img class="img-responsive" src="{$product->ImgUrl}" alt="">
+                <div class="caption-full">
+                    <h4 class="pull-right">${$product->Price}</h4>
+                    <h4><a>{$product->Name}</a>
+                        {if !($stock)}
+                        <span class="label label-danger">Not In Stock!</span>
+                        {else}
+                        <span class="label label-success">In Stock!</span>
+                        {/if}
+                    </h4>
+                    <blockquote>
+                        <p><i class="fa fa-quote-left"></i> {$product->DescriptionLong} <i
+                                class="fa fa-quote-right"></i></p>
+                    </blockquote>
+                    {if {$stock}}
+                    <form action="/catalogue/product={$product->Id}" method="post">
+                        <input type="hidden" name="item" value="{$product->Id}"/>
+                        <input type="hidden" name="name" value="{$product->Name}"/>
+                        <button type="submit" class="btn btn-default addbutton">
+                            <span class="glyphicon glyphicon-shopping-cart cart"></span>
+                            <span class="text">Add to cart</span>
+                        </button>
+                    </form>
+                    {/if}
+                    <div class="addToWishlist">
+
+                        <form action="/catalogue/product={$product->Id}" method="post">
+                            <input type="hidden" name="item" value="{$row->Id}"/>
+                            {if !($stock)}
+                            <span>This <strong>{$product->Name}</strong> is currently not in stock. Add it to your wishlist instead!</span>
+                            {else}
+                            <span>Don't want to buy this <strong>{$product->Name}</strong> yet? Add it to your wishlist!</span>
+                            {/if}
+                            <button type="submit" class="btn btn-warning">
+                                <span class="glyphicon glyphicon-list-alt"></span>
+                                <span class="text">Add to wishlist</span>
+                            </button>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
 
         </div>
-
     </div>
-
 </div>
-<!-- /.container -->
+{if $added === true}
+<script>
+    $(document).ready(function () {
+        toastr.options = {
+            "positionClass": "toast-bottom-right"
+        }
+        toastr.info("Item has been successfully added to the cart!", "Pokemart");
+    });
+</script>
+{/if}
--- View/admin/products.php
+++ View/admin/products.php
@@ -1,14 +1,11 @@
 <div class="container">
 
-    <h2>Admin</h2>
     <div class="row flat-header">
         <div class="col-sm-3 col-lg-3 ">
             <div class="header">Categories</div>
         </div>
         <div class="col-sm-9 col-lg-9">
-            <div class="header">{$title}
-
-            </div>
+            <div class="header">{$title}</div>
         </div>
     </div>
     <div class="row flat-lighterblue">
@@ -19,21 +16,21 @@
                 <div class="panel">
                     <div class="panel-heading">
                         <h4 class="panel-title">
-                            <a data-toggle="collapse" data-parent="#accordion" href="#collapse{$row->Id}">{$row->Category}</a>
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapse{$row->FoldId}">{$row->Category[0]}</a>
                         </h4>
                     </div>
-                    <div id="collapse{$row->Id}" class="panel-collapse collapse">
+                    <div id="collapse{$row->FoldId}" class="panel-collapse collapse">
                         <div class="panel-body category">
-                            <a href="/admin/p=cat/cat={$row->Category}">
-                                <div class="subcategory">
+                            <a href="/admin/p=cat/cat={$row->Category[1]}">
+                                <div class="subcategory all">
                                     All
                                 </div>
                             </a>
                             <hr class="small">
                             {foreach from=$row->SubCategories item=subcat}
-                            <a href="/admin/p=cat/subcat={$subcat}">
+                            <a href="/admin/p=cat/subcat={$subcat[1]}">
                                 <div class="subcategory">
-                                    {$subcat}
+                                    {$subcat[0]}
                                 </div>
                             </a>
                             {/foreach}
@@ -53,7 +50,7 @@
                 No items found.
                 {else}
                 {foreach from=$rows item=row}
-                <div class="col-xs-12 col-sm-12 col-lg-4 col-md-6">
+                <div class="col-xs-12 col-sm-12 col-lg-4 col-md-6" id="item{$row->Id}">
                     <div class="thumbnail">
                         <div class="imageWrapper">
                             <img src="{$row->ImgUrl}" alt="{$row->Name}">
@@ -61,22 +58,18 @@
                         <div class="info">
                             <div class="caption flat-caption">
                                 <h4 class="pull-right">${$row->Price}</h4>
-                                <h4><a href="/admin/p=cat/product={$row->Id}/p=cat">{$row->Name}</a>
+                                <h4><a href="/admin/p=cat/product={$row->Id}">{$row->Name}</a>
                                 </h4>
                                 <p>{$row->DescriptionShort}</p>
                             </div>
-
-                            <a class="btn toast-bottom-center" href="/admin/p=cat/product={$row->Id}"><span class="glyphicons glyphicons-pencil"></span>Edit Product</a>
+                            <a href="/admin/p=cat/product={$row->Id}" class="btn btn-default addbutton flat-button flat-lightblue">Edit</a>
                         </div>
                     </div>
                 </div>
                 {/foreach}
                 {/if}
             </div>
-
         </div>
-
     </div>
-
 </div>
 
